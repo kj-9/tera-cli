fc15afebca3937498cdb20486cba2a433365bd69
Kj <46800811+kj-9@users.noreply.github.com>
2024-04-02 23:27:51 +0900

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+//use std::io::{BufRead, Read};
+
+use clap::Parser;
+//use anyhow::{Context, Result};
+use tera::{Tera, Context};
+
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The path to the template file to read
+    template: std::path::PathBuf,
 }
+
+
+fn main() -> std::io::Result<()> {
+
+
+    let args = Cli::parse();
+
+    // bulk read file content
+    let content = std::fs::read_to_string(args.template)?;
+
+
+    let context = Context::new();
+    // add stuff to context
+    let result = Tera::one_off(&content, &context, false);
+
+    print!("{}", result.unwrap());
+
+    Ok(())
+
+}
+
+
+
